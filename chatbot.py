b679d6fb0335dd3d01e39c6556daf5fa0ab4928b
adurhmanmussbah <116857299+adurhmanmussbah@users.noreply.github.com>
2025-12-04 21:50:54 +0400

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -129,4 +129,5 @@ def webhook():
 
 if __name__ == "__main__":
     # 0.0.0.0 makes it accessible from outside (through cloudflared)
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
+
